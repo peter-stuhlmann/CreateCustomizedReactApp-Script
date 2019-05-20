312630b07a9da8592bd4bb705caa7b953ca6cfa0
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-05-21 00:00:33 +0200

    Add node-sass install command and customize function
    Node-sass will be installed after create-react-app;
    remove, add and customize some files of the React App;
    open the project in VS Code

--- react-app.sh
+++ react-app.sh
@@ -15,7 +15,48 @@ syntax_note() {
 install_modules() {
   printf "\n${CYAN}Install React App ...\n"
   npx create-react-app $1
+
+  printf "\n${CYAN}Install node-sass ...\n"
+  npm i node-sass --prefix $project_dir/
 }
+
+customize_react_app() {
+  printf "\n${CYAN}Customize the App ...\n"
+
+  # Delete files
+  rm $project_dir/src/index.js $project_dir/src/App.js $project_dir/src/App.test.js $project_dir/src/logo.svg $project_dir/src/App.css $project_dir/src/index.css $project_dir/README.md
+  
+  # Create folders
+  mkdir $project_dir/src/components/ $project_dir/src/assets/ $project_dir/src/assets/scss/ $project_dir/src/assets/img/
+  
+  # Create files 
+  touch $project_dir/src/assets/scss/main.scss $project_dir/src/index.js $project_dir/src/components/App.js 
+
+  # Write default code in files
+
+  echo 'import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+ReactDOM.render(<App />, document.querySelector("#root"));' >>$project_dir/src/index.js
+
+  echo 'import React, { Component } from "react";
+import "../assets/scss/main.scss"
+
+export default class App extends Component {
+
+  render() {
+    return ( 
+      <React.Fragment>
+        Hello World!
+      </React.Fragment>
+    )
+  }
+
+}' >>$project_dir/src/components/App.js
+}
+
 
 if [[ $# -lt 1 ]]; then
   syntax_note
@@ -24,6 +65,10 @@ else
 
   install_modules $project_dir
 
+  customize_react_app
+
   printf "\n${CYAN}Done!\n"
+
+  code $project_dir/
 
 fi

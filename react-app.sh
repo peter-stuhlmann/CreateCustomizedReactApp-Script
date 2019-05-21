8072dcffbc86ab763f6ed7810f28868d61650864
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-05-22 00:09:13 +0200

    Add name variable
    Create a variable NAME that contains the name of the user and enters it in the license.

--- react-app.sh
+++ react-app.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+NAME='<your-name>'
+
 # colors
 CYAN='\033[0;36m'
 GREEN='\033[0;32m'
@@ -85,12 +87,12 @@ else
   if [[ ${#add_files[@]} -gt 0 ]]; then
     printf "\n${CYAN}Create ${GREEN}${#add_files[@]}${COLORRESET}\n"
     touch $project_dir/${add_files[@]} 
-    echo 'MIT License
+    echo "MIT License
 
-Copyright (c) 2019 <your name>
+Copyright (c) 2019 ${NAME}
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
+of this software and associated documentation files (the 'Software'), to deal
 in the Software without restriction, including without limitation the rights
 to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 copies of the Software, and to permit persons to whom the Software is
@@ -99,13 +101,13 @@ furnished to do so, subject to the following conditions:
 The above copyright notice and this permission notice shall be included in all
 copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
 AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.' >>$project_dir/LICENSE
+SOFTWARE." >>$project_dir/LICENSE
 
   fi
 

452310f0ba435d5a29d4e8c5b9474e61740ef580
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-05-21 15:46:59 +0200

    Add first git steps
    Add these steps:
    1. git init
    2. git add .
    3. git commit -m ...

--- react-app.sh
+++ react-app.sh
@@ -15,19 +15,20 @@ syntax_note() {
 }
 
 install_modules() {
-  printf "\n${CYAN}Install React App ...\n"
+  printf "\n${CYAN}Install React App ...${COLORRESET}\n"
   npx create-react-app $1
 
-  printf "\n${CYAN}Install node-sass ...\n"
+  printf "\n${CYAN}Install node-sass ...${COLORRESET}\n"
   npm i node-sass --prefix $project_dir/
 }
 
 customize_react_app() {
-  printf "\n${CYAN}Customize the App ...\n"
+  printf "\n${CYAN}Customize the App ...${COLORRESET}\n"
 
   # Delete files
   rm $project_dir/src/index.js $project_dir/src/App.js $project_dir/src/App.test.js $project_dir/src/logo.svg $project_dir/src/App.css $project_dir/src/index.css $project_dir/README.md
-  
+  rm -rf $project_dir/.git
+
   # Create folders
   mkdir $project_dir/src/components/ $project_dir/src/assets/ $project_dir/src/assets/scss/ $project_dir/src/assets/img/
   
@@ -59,7 +60,6 @@ export default class App extends Component {
 }' >>$project_dir/src/components/App.js
 }
 
-
 if [[ $# -lt 1 ]]; then
   syntax_note
 else
@@ -82,8 +82,26 @@ else
 
   customize_react_app
 
-  printf "\n${CYAN}Done!\n"
+  printf "\n${CYAN}Done!${COLORRESET}\n"
 
   code $project_dir/
+
+  cd $project_dir
+
+  git init
+  git add .
+  git commit -m "Init React project" -m "Install modules:
+- npm i create-react-app
+- npm i node-sass (to use scss)
+
+Customize React App:
+- Create two folders in src: components/ and assets/ respectively assets/scss/
+- Move App.js in components/ and add a class App with an empty render function
+- Create empty main.scss in assets/scss/ and import it in App.js
+
+Remove unneeded files:
+- Remove logo.svg from src/ and the logo import in index.js"
+
+  exec bash
 
 fi

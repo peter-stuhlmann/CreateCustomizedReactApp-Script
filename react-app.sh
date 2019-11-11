90c281b8dc0e466554f949193828a86c4f6e0e61
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-11-11 23:01:10 +0100

    Update content of created files

--- react-app.sh
+++ react-app.sh
@@ -23,7 +23,7 @@ customize_react_app() {
   printf "\n${CYAN}Customize the App ...${COLORRESET}\n"
 
   # Delete files
-  rm $project_dir/src/index.js $project_dir/src/App.js $project_dir/src/App.test.js $project_dir/src/logo.svg $project_dir/src/App.css $project_dir/src/index.css $project_dir/README.md
+  rm $project_dir/src/index.js $project_dir/src/App.js $project_dir/src/App.test.js $project_dir/src/logo.svg $project_dir/src/App.css $project_dir/src/index.css $project_dir/src/serviceWorker.js $project_dir/README.md
   rm -rf $project_dir/.git
 
   # Create folders
@@ -37,25 +37,19 @@ customize_react_app() {
   echo 'import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+ReactDOM.render(<App />, document.querySelector("#root"));' >>$project_dir/src/index.js
 
-serviceWorker.unregister();' >>$project_dir/src/index.js
-
-  echo 'import React, { Component } from "react";
-import "../assets/scss/main.scss"
+  echo 'import React, { Component, Fragment } from "react";
 
 export default class App extends Component {
-
   render() {
     return ( 
-      <React.Fragment>
+      <Fragment>
         Hello World!
-      </React.Fragment>
+      </Fragment>
     )
   }
-
 }' >>$project_dir/src/components/App.js
 }
 
@@ -79,15 +73,7 @@ else
 
   git init
   git add .
-  git commit -m "Init React project" -m "Install create-react-app
-
-Customize React App:
-- Create two folders in src: components/ and assets/ respectively assets/scss/
-- Move App.js in components/ and add a class App with an empty render function
-- Create empty main.scss in assets/scss/ and import it in App.js
-
-Remove unneeded files:
-- Remove logo.svg from src/ and the logo import in index.js"
+  git commit -m "Init React app"
 
   if [[ ${#npm_pkgs[@]} -gt 0 ]]; then
     printf "\n${CYAN}Install ${GREEN}${npm_pkgs[@]}\n"
